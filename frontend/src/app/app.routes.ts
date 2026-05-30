cbe05c4c2637ee0c8a2a2e92da30e6588dfcaf95
Yoann <85394669+Lallethu@users.noreply.github.com>
2026-05-31 00:08:03 +0200

    feat(dashboard): handle the routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,4 +3,7 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
 	{ path: '', component: DashboardComponent },
+	// { path: 'detail/:id', component: DetailComponent },
+	// { path: 'favorites', component: FavoritesComponent },
+	{ path: '**', redirectTo: '' }
 ];
