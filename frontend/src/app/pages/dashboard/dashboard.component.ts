bc36cd9df2bba25133c3c3f7e59c71c77488dabf
Yoann <85394669+Lallethu@users.noreply.github.com>
2026-05-31 00:11:26 +0200

    feat(dashboard): loads the list of coins

---
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,30 @@
+import { Component, inject, signal, OnInit } from '@angular/core';
+import { CryptoService } from '../../core/services/crypto.service'
+import type { Crypto } from '../../core/models/crypto.model';
+import { CryptoCardComponent } from "../../shared/components/crypto-card/crypto-card.component";
+
+@Component({
+	templateUrl: './dashboard.html',
+	styleUrl: './dashboard.scss',
+	imports: [CryptoCardComponent]
+})
+export class DashboardComponent implements OnInit {
+	private crypto = inject(CryptoService)
+	coins = signal<Crypto[]>([])
+	isLoading = signal(true)
+
+	ngOnInit(): void {
+		this.loadMarkets()
+	}
+
+	loadMarkets(): void {
+    this.isLoading.set(true)
+    this.crypto.getMarkets().subscribe({
+      next: (coins) => {
+        this.coins.set(coins)
+        this.isLoading.set(false)
+      },
+      error: () => this.isLoading.set(false)
+    })
+  }
+}
