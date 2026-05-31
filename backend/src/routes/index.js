e21cada7535df0516729b109f916afbfb1944b56
Yoann <85394669+Lallethu@users.noreply.github.com>
2026-05-31 19:37:53 +0200

    feat(favorite): plug the favorites route to the router

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import pingRouter from './ping.route.js'
 import cryptoRouter from './crypto.route.js'
+import favoritesRouter from './favorites.route.js'
 
 const router = Router()
 
 router.use('/ping', pingRouter)
 router.use('/crypto', cryptoRouter)
+router.use('/favorites', favoritesRouter)
 
 export default router
