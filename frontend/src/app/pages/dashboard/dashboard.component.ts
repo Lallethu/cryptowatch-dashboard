3776065144f52730b7fea1d29620adf0905df200
Yoann <85394669+Lallethu@users.noreply.github.com>
2026-05-31 19:43:57 +0200

    feat(favorite): add the spinner component

--- frontend/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -2,11 +2,12 @@ import { Component, inject, signal, OnInit } from '@angular/core';
 import { CryptoService } from '../../core/services/crypto.service'
 import type { Crypto } from '../../core/models/crypto.model';
 import { CryptoCardComponent } from "../../shared/components/crypto-card/crypto-card.component";
+import { SpinnerComponent } from "../../shared/components/spinner/spinner.component";
 
 @Component({
 	templateUrl: './dashboard.html',
 	styleUrl: './dashboard.scss',
-	imports: [CryptoCardComponent]
+	imports: [CryptoCardComponent, SpinnerComponent]
 })
 export class DashboardComponent implements OnInit {
 	private crypto = inject(CryptoService)
