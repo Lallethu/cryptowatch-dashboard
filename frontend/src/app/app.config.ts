a38b83cc7e8b4f3e3740747c81d6d8241b0707d1
Yoann <85394669+Lallethu@users.noreply.github.com>
2026-05-31 15:14:02 +0200

    feat(detail): add the chart provider to the app config to use charts

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,11 +1,16 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+	provideCharts,
+	withDefaultRegisterables,
+} from 'ng2-charts';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+	providers: [
+		provideBrowserGlobalErrorListeners(),
+		provideRouter(routes),
+		provideCharts(withDefaultRegisterables())
+	]
 };
