403bda8f96f7efed087081d578b33acb6e824664
Yoann <85394669+Lallethu@users.noreply.github.com>
2026-05-31 15:15:46 +0200

    feat(detail): refacto getById to plug on the router in the backend

--- frontend/src/app/core/services/crypto.service.ts
+++ frontend/src/app/core/services/crypto.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core'
 import { map, Observable } from 'rxjs'
 import { BaseApiService } from './base-api.service'
 import { ApiResponse } from '../models/api.model'
-import type { Crypto } from '../models/crypto.model'
+import type { Crypto, CryptoDetail } from '../models/crypto.model'
 
 @Injectable({ providedIn: 'root' })
 export class CryptoService extends BaseApiService {
@@ -15,7 +15,7 @@ export class CryptoService extends BaseApiService {
 		return this.get<ApiResponse<Crypto[]>>('/getMarkets').pipe(map(res => res.data))
 	}
 
-	getById(id: string): Observable<Crypto> {
-		return this.get<Crypto>(`/crypto/${id}`)
+	marketChart(id: string): Observable<CryptoDetail> {
+		return this.get<ApiResponse<CryptoDetail>>(`/market_chart/${id}`).pipe(map(res => res.data))
 	}
 }
