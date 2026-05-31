d65e1acdcb627c4f47fddb486ad609ad762aa473
Yoann <85394669+Lallethu@users.noreply.github.com>
2026-05-31 15:12:01 +0200

    feat(detail): add service method to get specific market value from seven day to today

--- backend/src/services/coingecko.service.js
+++ backend/src/services/coingecko.service.js
@@ -1,6 +1,8 @@
 import { config } from '../config/index.js'
 import { AppError } from '../middleware/errorHandler.js'
 
+const SEVEN_DAYS_TIMESTAMP = 7 * 24 * 60 * 60 * 1000
+
 export const CoinGeckoService = {
   getMarkets: async () => {
     const searchParams = new URLSearchParams({
@@ -13,6 +15,33 @@ export const CoinGeckoService = {
 
     const response = await fetch(
       `${config.coingeckoapi.base_url}/coins/markets?${searchParams.toString()}`,
+      {
+        headers: {
+          'x-cg-demo-api-key': config.coingeckoapi.key,
+        },
+      }
+    )
+
+    if (!response.ok) throw new AppError(response.error.message, response.status)
+
+    return response.json()
+  },
+
+  marketChart: async id => {
+    const to = Date.now()
+    const from = Date.now() - SEVEN_DAYS_TIMESTAMP
+
+    const searchParams = new URLSearchParams({
+      vs_currency: 'usd',
+      interval: 'daily',
+      from: from,
+      to: to,
+      per_page: 7,
+      page: 1,
+    })
+
+    const response = await fetch(
+      `${config.coingeckoapi.base_url}/coins/${id}/market_chart/range?${searchParams.toString()}`,
       {
         headers: {
           'x-cg-demo-api-key': config.coingeckoapi.key,
